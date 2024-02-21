60f57808b0210e7d311bdf51fb46b8f4e131185c
MkGodh <fenryrvla@yandex.ru>
2024-02-21 14:18:13 +0300

    Добавьте файлы проекта.

---
+++ AgeInBar/AgeInBar.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+
+    int currentDay;
+    int currentMonth;
+    int currentYear;
+    int day;
+    int month;
+    int year;
+
+    std::cout << "Добро пожаловать в бар Синяя борода!\n";
+    std::cout << "Введите сегодняшний день, месяц и год: "; std::cin >> currentDay >> currentMonth >> currentYear;
+    std::cout << "Введите ваш день, месяц и год рождения: "; std::cin >> day >> month >> year;
+    if (currentYear - year >= 18 && month <= currentMonth && currentDay - day >= 1) {
+        std::cout << "Зачем тебе пиво? Выпей лучше Текиллу!";
+    }
+    else {
+        std::cout << "Бухалка не выросла! Пей сок браток!";
+    }
+}
+
